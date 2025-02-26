f8b79a73fa3ae8c8d945dd0e2571119d92fa1fb1
nbchamod <lakshanchamod473@gmail.com>
2025-02-27 01:00:15 +0530

    Create one part of ASSIGED WORKS in technician dashboard backend

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createAppointment, getAppointments, updateWorkload, suggestionWrite } = require("../controllers/appointmentController.js");
+const { createAppointment, getAppointments, updateWorkload, suggestionWrite, PutTechnicianConfirm, PutTechnicianDecline } = require("../controllers/appointmentController.js");
 const { assignTechnician } = require("../controllers/assignTechnician.js");
 const { updateAppointmentStatus } = require("../controllers/statusUpdate.js");
 
@@ -9,8 +9,9 @@ router.post("/", createAppointment);
 router.get("/", getAppointments);
 router.put("/:id/workload", updateWorkload);
 
-router.put("/:appointmentId/assign", assignTechnician);
+router.put("/:appointmentId/assign2", assignTechnician);
 
 router.put("/:appointmentId/statusUpdate", updateAppointmentStatus);
 router.put("/:appointmentId/suggestions", suggestionWrite);
+
 module.exports = router;
