3d72755238649e8ce846af45264b1570f173565d
Asry18 <asryamaz000@gmail.com>
2025-03-10 12:13:24 +0530

    when submitting a form the vehicle Id must be generarted from the appointment , thats not related to vehicle dataset

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,21 +1,16 @@
 const mongoose = require("mongoose");
 
 const appointmentSchema = new mongoose.Schema({
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User", // Reference to the User model
+            required: true,
+        },
 
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User", // Reference to the User model
-        required: true
-    },
-    vehicleId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Vehicle", // Reference to the Vehicle model
-        required: true
-    },
-
+        vehicleId: { type: String },
         vehicleNumber: { type: String, required: true },
         model: { type: String, required: true },
-        issue: { type: String, required: false  },    
+        issue: { type: String, required: false },
         status: {
             type: String,
             enum: [
@@ -39,32 +34,29 @@ const appointmentSchema = new mongoose.Schema({
                 default: "Pending",
             }, // Optional
         }, ],
-        services: [{ 
-            type: String, 
-            enum: ["A/C repair", "Oil repair", "Break Services"], 
-            required: true 
-        }],
-            preferredTime: { type: String, required: true },
-            expectedDeliveryDate: { type: Date, required: true },
-            
-            workload: { type: String, default: "123" },
-            tech: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Technician", //model name not the collection name
-                default: null,
-            },
-            techMessage: { type: String, default: null },
-            suggestion: { type: String, default: null },
-            contactNumber: { type: String },
-    
-            budgetId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Budget",
-                default: null,
-            },
-        }, { timestamps: true } // Auto-adds createdAt & updatedAt
-          
-        
+        services: [{
+            type: String,
+            enum: ["A/C repair", "Oil repair", "Break Services"],
+            required: true,
+        }, ],
+        preferredTime: { type: String, required: true },
+        expectedDeliveryDate: { type: Date, required: true },
+
+        tech: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Technician", //model name not the collection name
+            default: null,
+        },
+        techMessage: { type: String, default: null },
+        suggestion: { type: String, default: null },
+        contactNumber: { type: String },
+
+        budgetId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Budget",
+            default: null,
+        },
+    }, { timestamps: true } // Auto-adds createdAt & updatedAt
 );
 
 const Appointment = mongoose.model("Appointment", appointmentSchema);
