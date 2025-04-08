951f0cb2c42ed8a52f317cd8025e03692a3789e9
vijayanga <vijayanga123@gmail.com>
2025-04-08 10:25:14 +0530

    feedback route add

---
+++ routes/feedbackRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  getFeedbacks,
+  addReply,
+  updateActionStatus,
+  deleteFeedback,
+} = require("../controllers/feedbackController");
+
+const router = express.Router();
+
+router.get("/", getFeedbacks);
+router.put("/:id/reply", addReply);
+router.put("/:id/action", updateActionStatus);
+router.put("/:id/delete", deleteFeedback); // Soft delete route
+
+module.exports = router;
