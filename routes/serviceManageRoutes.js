5a54ac2ec3ccfe267bb0eec9c1bf0b09d8be33e3
vijayanga <vijayanga123@gmail.com>
2025-04-12 23:35:00 +0530

    service manage route add service

--- routes/serviceManageRoutes.js
+++ routes/serviceManageRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { getAllServices } = require("../controllers/serviceControllerManage");
+const {
+  getAllServices,
+  addService,
+} = require("../controllers/serviceControllerManage");
 
 const router = express.Router();
 
 // Fetch all services
 router.get("/", getAllServices);
+
+// Add a new service
+router.post("/", addService);
 
 module.exports = router;
