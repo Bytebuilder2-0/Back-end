7085610f5ab07913bf715fd0ccaf94766b5b9c15
vijayanga <vijayanga123@gmail.com>
2025-04-12 23:11:10 +0530

    model service manage controller v3

--- controllers/serviceControllerManage.js
+++ controllers/serviceControllerManage.js
@@ -23,3 +23,25 @@ exports.addService = async (req, res) => {
 };
 
 // Toggle the selection state of a service
+exports.toggleService = async (req, res) => {
+  try {
+    const updatedService = await ServiceManage.findByIdAndUpdate(
+      req.params.id,
+      { selected: req.body.selected },
+      { new: true }
+    );
+    res.status(200).json(updatedService);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Delete a service
+exports.deleteService = async (req, res) => {
+  try {
+    await ServiceManage.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Service deleted successfully" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
