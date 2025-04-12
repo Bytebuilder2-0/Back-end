aeec6885c933222a91996a94523c7f7d7d25ed9a
vijayanga <vijayanaga123@gmail.com>
2025-04-12 23:36:06 +0530

    service manage route update service

--- routes/serviceManageRoutes.js
+++ routes/serviceManageRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   getAllServices,
   addService,
+  updateService,
 } = require("../controllers/serviceControllerManage");
 
 const router = express.Router();
@@ -11,5 +12,7 @@ router.get("/", getAllServices);
 
 // Add a new service
 router.post("/", addService);
+
+router.put("/update/:id", updateService);
 
 module.exports = router;
