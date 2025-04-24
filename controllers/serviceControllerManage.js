a7bf9ddd3cdaf6454bd91549bf6e5fe568c576d2
vijayanga <vijayanga123@gmail.com>
2025-04-24 09:40:04 +0530

    service mange controller update

--- controllers/serviceControllerManage.js
+++ controllers/serviceControllerManage.js
@@ -1,7 +1,14 @@
 const ServiceManage = require("../models/serviceManage");
 
 // Fetch all services
-exports.getAllServices = async (req, res) => {};
+exports.getAllServices = async (req, res) => {
+  try {
+    const services = await ServiceManage.find();
+    res.status(200).json(services);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
 
 // Add a new service
 exports.addService = async (req, res) => {
