fa0ed5df9766cfc6f01596b5d41f98914d785c41
vijayanga <vijayanga123@gmail.com>
2025-04-24 09:44:22 +0530

    feedback mange model

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -8,4 +8,9 @@ const {
 
 const router = express.Router();
 
+router.get("/", getFeedbacks);
+router.put("/:id/reply", addReply);
+router.put("/:id/action", updateActionStatus);
+router.put("/:id/delete", deleteFeedback); // Soft delete route
+
 module.exports = router;
