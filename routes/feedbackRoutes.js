800f9a6ee01bc0768112d400348fd1529ec1f150
vijayanga <vijayanga123@gmail.com>
2025-04-24 09:42:34 +0530

    feedback manage

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -8,9 +8,4 @@ const {
 
 const router = express.Router();
 
-router.get("/", getFeedbacks);
-router.put("/:id/reply", addReply);
-router.put("/:id/action", updateActionStatus);
-router.put("/:id/delete", deleteFeedback); // Soft delete route
-
 module.exports = router;
