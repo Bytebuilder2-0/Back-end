26b3e541d4ffff1bcf8f59cb4c36a40b85c59e9c
vijayanga <vijayanga123@gmail.com>
2025-04-28 18:59:16 +0530

    error recover

--- server.js
+++ server.js
@@ -8,13 +8,12 @@ const appointmentRoutes = require("./routes/appointmentRoutes.js");
 const technicianRoutes = require("./routes/technicianRoutes.js");
 const budgetRoutes = require("./routes/budgetRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
+const serviceRoutes = require("./routes/serviceRoutes.js");
 
 const authRoutes = require("./routes/authRouter.js");
 const { authMiddleware } = require("./middlewares/userAuthMiddleware.js");
 const feedbackRoutes = require("./routes/feedbackRoutes");
 const serviceManage = require("./routes/serviceManageRoutes.js");
-
-
 
 dotenv.config();
 connectDB();
@@ -25,11 +24,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-
 //Routes
-app.use("/api/auth", authRoutes);
-app.use(authMiddleware);
+//app.use("/api/auth", authRoutes);
+//app.use(authMiddleware);
 //app.use("/api/helloworld", (req, res) => res.send("Hello World")) // Apply the auth middleware to all routes below this line
 app.use("/api/appointments", appointmentRoutes);
 app.use("/api/technicians", technicianRoutes);
@@ -37,6 +34,7 @@ app.use("/api/budget", budgetRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/feedback", feedbackRoutes);
 
+app.use("/api/services", serviceRoutes);
 
 app.use("/api/servicesManage", serviceManage);
 
