46f5f53e6767778d5b8b098bec1ba16f8ccfe4a0
vijayanga <vijayanga123@gmail.com>
2025-04-29 11:13:54 +0530

    before pull commit

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const appointmentRoutes = require("./routes/appointmentRoutes.js");
 const technicianRoutes = require("./routes/technicianRoutes.js");
 const budgetRoutes = require("./routes/budgetRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
-//const serviceRoutes = require("./routes/serviceRoutes.js");
 
 const authRoutes = require("./routes/authRouter.js");
 const { authMiddleware } = require("./middlewares/userAuthMiddleware.js");
@@ -25,16 +24,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 //Routes
-//app.use("/api/auth", authRoutes);
-//app.use(authMiddleware);
+app.use("/api/auth", authRoutes);
+app.use(authMiddleware);
 //app.use("/api/helloworld", (req, res) => res.send("Hello World")) // Apply the auth middleware to all routes below this line
 app.use("/api/appointments", appointmentRoutes);
 app.use("/api/technicians", technicianRoutes);
 app.use("/api/budget", budgetRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/feedback", feedbackRoutes);
-
-//app.use("/api/services", serviceRoutes);
 
 app.use("/api/servicesManage", service);
 
