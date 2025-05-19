ca010e291503b72b294b28bab6121b494fb11564
Asry18 <asryamaz000@gmail.com>
2025-05-19 22:03:54 +0530

    supervisor role name correction

--- models/auth.js
+++ models/auth.js
@@ -3,54 +3,54 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
-      type: String,
-      required: true,
-      unique: true,
+        type: String,
+        required: true,
+        unique: true,
     },
     fullName: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     userName: {
         type: String,
         required: true,
-      },
+    },
     phone: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     password: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     role: {
-      type: String,
-      required: true,
-      enum: ["customer", "technician", "manager", "Supervisor", "admin"],
-      default: "customer",
-      technicianId: { type: String },
-      managerId: { type: String },
-      supervisorId: { type: String },
+        type: String,
+        required: true,
+        enum: ["customer", "technician", "manager", "supervisor", "admin"],
+        default: "customer",
+        technicianId: { type: String },
+        managerId: { type: String },
+        supervisorId: { type: String },
 
     },
 
     isDisabled: {
-      type: Boolean,
-      required: true,
-      default: false,
+        type: Boolean,
+        required: true,
+        default: false,
     },
     isEmailVerified: {
-      type: Boolean,
-      required: true,
-      default: false,
+        type: Boolean,
+        required: true,
+        default: false,
     },
     resetPasswordToken: String,
     resetPasswordExpires: Date,
     verificationToken: String,
     verificationTokenExpires: Date,
-    
-  },{timestamps: true});
 
-  const Auth = mongoose.model('auth', userSchema);
+}, { timestamps: true });
 
-  module.exports = Auth;
+const Auth = mongoose.model('auth', userSchema);
+
+module.exports = Auth;
