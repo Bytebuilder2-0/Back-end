9f308d1cf9f9fe5278332c44e0b42f55ddad0def
vijayanga <vijayanga123@gmail.com>
2025-07-15 21:12:02 +0530

    changes

--- controllers/feedBackDisplay.js
+++ controllers/feedBackDisplay.js
@@ -1,14 +1,14 @@
 const crypto = require("crypto"); // Importing the crypto module
-
 const Feedback = require("../models/Feedback");
 
-// Define md5 function
+// Define md5 function (if using Gravatar, but not needed for UI Avatars)
 function md5(string) {
   return crypto.createHash("md5").update(string).digest("hex");
 }
 
 const getFeedbacks = async (req, res) => {
   try {
+    // Fetch feedback data
     const feedbacks = await Feedback.find({
       actionStatus: "yes",
       deleted: false,
@@ -18,7 +18,7 @@ const getFeedbacks = async (req, res) => {
       select: "_id",
       populate: {
         path: "userId",
-        select: "name email",
+        select: "name email", // Make sure the userId object includes name and email
       },
     });
 
@@ -30,7 +30,13 @@ const getFeedbacks = async (req, res) => {
       });
     }
 
+    // Format feedbacks
     const formattedFeedbacks = feedbacks.map((feedback) => {
+      // Generate a fallback avatar using UI Avatars (based on username or email)
+      const avatarUrl = `https://ui-avatars.com/api/?name=${encodeURIComponent(
+        feedback.appointmentId?.userId?.name || "User"
+      )}&background=random&color=fff&font-size=0.3`;
+
       return {
         id: feedback._id,
         feedbackId: feedback.feedbackId,
@@ -38,9 +44,7 @@ const getFeedbacks = async (req, res) => {
         userComment: feedback.comment,
         adminReply: feedback.reply,
         feedbackDate: feedback.feedbackDate,
-        avatarUrl: `https://www.gravatar.com/avatar/${md5(
-          (feedback.appointmentId?.userId?.email || "").toLowerCase().trim()
-        )}?d=identicon`,
+        avatarUrl: avatarUrl, // Pass the avatar URL to frontend
       };
     });
 
