b0d25ad51e92c916065f350d72bfa02167e9292f
Gayani2002 <gunawardhanagwgm.22@uom.lk>
2025-07-15 14:27:25 +0530

    Feedback submission

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -5,15 +5,20 @@ const {
   submitFeedback,
   updateActionStatus,
   deleteFeedback,
+  // getUserFeedbacks
 } = require("../controllers/feedbackController");
 // feedback model
 
 const router = express.Router();
 
 router.post("/:id/submit", submitFeedback);
+
 router.get("/", getFeedbacks);
+// router.get("/user/:id", getUserFeedbacks);
+
 router.put("/:id/reply", addReply);
 router.put("/:id/action", updateActionStatus);
 router.put("/:id/delete", deleteFeedback); // Soft delete route
+
 
 module.exports = router;
