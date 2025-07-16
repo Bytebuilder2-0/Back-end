7d26a2cd217e03bb4613a534fbaa7219787d28bf
Gayani2002 <gunawardhanagwgm.22@uom.lk>
2025-07-17 03:44:23 +0530

    Some change & feedback history

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -86,32 +86,44 @@ const submitFeedback = async (req, res) => {
   }
 };
 
-// const getUserFeedbacks = async (req,res) =>{
+const getUserFeedbacks = async (req,res) =>{
+  
+  try{
 
-//   try{
-//     const { id } = req.params;
+     const { id } = req.params;
+    const appointment = await Appointment.findOne({
+      _id: id,
+      feedbackStatus: true
+    }).populate('feedbackId'); 
 
-//     if (!mongoose.Types.ObjectId.isValid(userId)) {
-//       return res.status(400).json({
-//         success: false,
-//         message: "Invalid user ID format",
-//       });
-//     }    
-//     const userObjectId = new mongoose.Types.ObjectId(userId);
+   if (!appointment) {
+      return res.status(404).json({ message: 'Appointment not found' });
+    }  
     
-//     const userExists = await User.exists({ _id: userObjectId });
-//     if (!userExists) {
-//       console.log(`User ${userId} not found`);
-//       return res.status(404).json({
-//         success: false,
-//         message: "User not found",
-//       });
-//     }
+    const feedback = appointment.feedbackId;
 
+     if (!feedback) {
+      return res.status(404).json({ message: 'Feedback details not found' });
+    }
 
+    res.json({
+      success: true,
+      data: feedback,
+      appointmentDetails: {
+        preferredDate : appointment.preferredDate,
+        vehicleNumber: appointment.vehicleNumber,
+        model: appointment.model,
+        services: appointment.services,
+        status: appointment.status
+      }
 
-// }
-// };
+    });
+}
+ catch (error) {
+    console.error('Error fetching feedback:', error);
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
 
 // after email create i was add  agian to
 const addReply = async (req, res) => {
@@ -161,5 +173,5 @@ module.exports = {
   addReply,
   updateActionStatus,
   deleteFeedback,
-  // getUserFeedbacks
+  getUserFeedbacks
 };
